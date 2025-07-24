ce907d364f8c63e58b301bc46f7c983ec8c18863
David Martinez <414313755@qq.com>
2025-07-24 17:24:59 +0800

    merge: LDC1101-Configuration finished and online.

---
+++ light up/Core/Inc/oledfont.h
@@ -0,0 +1,21 @@
+/*
+ * oledfont.h
+ *
+ *  Created on: 2022年8月16日
+ *      Author: HP
+ */
+
+
+#ifndef OLED_OLEDFONT_H_
+#define OLED_OLEDFONT_H_
+
+extern const unsigned char F6x8[][6];
+extern const unsigned char F8X16[];
+extern const unsigned char Hzk[][32];
+extern unsigned char BMP1[];
+//extern unsigned char BMP2[].........
+#endif /* OLED_OLEDFONT_H_ */
+
+//————————————————
+//版权声明：本文为CSDN博主「想要亿只独角兽」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+//原文链接：https://blog.csdn.net/LYH6767/article/details/126032948
---
+++ light up/MDK-ARM/light up/oledfont.d
@@ -0,0 +1,2 @@
+light up\oledfont.o: ..\Core\Src\oledfont.c
+light up\oledfont.o: ../Core/Inc/oledfont.h
