b09c9b27f660bd697848e9698ace4878d3ddf9f6
David Martinez <414313755@qq.com>
2025-09-21 16:44:22 +0800

    clean(Viscometer):Delete VSCode & EIDE files.

---
+++ light up/MDK-ARM/light up/oled_data.d
@@ -0,0 +1,3 @@
+light up\oled_data.o: ..\Core\Src\OLED_Data.c
+light up\oled_data.o: ../Core/Inc/OLED_Data.h
+light up\oled_data.o: D:\Keil528a\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ light up/MDK-ARM/light up/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+light up\startup_stm32f103xb.o: startup_stm32f103xb.s
